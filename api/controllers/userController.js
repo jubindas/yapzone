27f7eca1be2b53d2377df27c18126d876c3fa3ab
jubindas <jubinrohidas78@gmail.com>
2025-07-03 21:39:30 +0530

    update: login api updated

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,65 +1,64 @@
-
-import User from '../models/User.js';
-import mongoose from 'mongoose';
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
 
 // Create a new user
-const createUser = async(req, res)=>{
-    try{
-//if user already exists, return error
+const createUser = async (req, res) => {
+  try {
+    //if user already exists, return error
 
-const userExists = await User.findOne({email: req.body.email});
-if(userExists){
-    return res.status(400).send({
+    const userExists = await User.findOne({ email: req.body.email });
+    if (userExists) {
+      return res.status(400).send({
         message: "user already exists",
-        error: "user already exists"
+        error: "user already exists",
+      });
+    }
+
+    // Create a new user
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
     });
-}
 
-// Create a new user
-const user = new User({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    email: req.body.email,
-    password: req.body.password,
-});
-
-const newUser = await user.save();
-res.send({
-    message: "user created successfully",
-    user: {         
+    const newUser = await user.save();
+    res.send({
+      message: "user created successfully",
+      user: {
         id: newUser._id,
         firstName: newUser.firstName,
         lastName: newUser.lastName,
-        email: newUser.email,   
-    }
-});
+        email: newUser.email,
+      },
+    });
+  } catch (error) {
+    res.send({
+      messsage: "error creating user",
+      error: error.message,
+    });
+  }
+};
 
-    }catch(error){
-        res.send({
-            messsage: "error creating user",
-            error: error.message
-        })
-    }
-
-}
-
-const login = async(req, res) => {
+const login = async (req, res) => {
   const { email, password } = req.body;
-  
 
   if (!email || !password) {
     return res.status(400).send({
       message: "email and password are required",
-      error: "email and password are required"
+      error: "email and password are required",
     });
   }
 
   try {
     const user = await User.findOne({ email });
-    
 
     if (user && user.password === password) {
-      
+      const toket = jwt.sign({ id: user._id }, process.env.SECRETKEY, {
+        expiresIn: "1h",
+      });
+      console.log("Login successful ✅");
+
       return res.status(200).send({
         message: "user logged in successfully",
         user: {
@@ -67,26 +66,22 @@ const login = async(req, res) => {
           firstName: user.firstName,
           lastName: user.lastName,
           email: user.email,
-        }
+        },
       });
     } else {
       console.log("Login failed ❌ - Wrong email or password");
       return res.status(400).send({
         message: "invalid email or password",
-        error: "invalid email or password"
+        error: "invalid email or password",
       });
     }
-
   } catch (error) {
     console.log("Login error ❌", error.message);
     return res.status(400).send({
       message: "error logging in user",
-      error: error.message
+      error: error.message,
     });
   }
 };
 
-
-
-
- export {createUser, login }
+export { createUser, login };
---
+++ client/src/users/signup/Signup.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Signup() {
+  return (
+    <div>
+      <form action="">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="text" placeholder="username" />
+        <input type="email" placeholder="email" />
+        <input type="password" placeholder="Password" />
+        <button type="submit">signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
