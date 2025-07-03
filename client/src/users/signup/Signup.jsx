382fc7f075185a1875c68ffc61554948c4600250
jubindas <jubinrohidas78@gmail.com>
2025-07-03 21:49:49 +0530

    login: updated

--- client/src/users/signup/Signup.jsx
+++ client/src/users/signup/Signup.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Signup() {
+  const [formData, setFormData] = useState("");
+
+  const onChangee = (e) => {
+    setFormData({ firstname: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form submitted with data:", formData);
+  };
+
   return (
     <div>
-      <form action="">
-        <input type="text" placeholder="First Name" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="firstname"
+          value={formData.firstname}
+          onChange={onChangee}
+          placeholder="First Name"
+        />
         <input type="text" placeholder="Last Name" />
         <input type="text" placeholder="username" />
         <input type="email" placeholder="email" />
